22e1f7dbe1184a039312ea59d6a88156b13629a5
Bruno Bonfils <bbonfils@gmail.com>
2019-10-21 00:04:55 +0200

    WIP

--- openid_connect/home.py
+++ openid_connect/home.py
@@ -1,25 +1,22 @@
 from flask import Flask, url_for, jsonify
-from flask_oauthlib.client import OAuth
 import os
+from flask_pyoidc import OIDCAuthentication
+from flask_pyoidc.provider_configuration import ProviderConfiguration, ClientRegistrationInfo
+from flask_pyoidc.user_session import UserSession
 
 app = Flask(__name__)
+app.config['SERVER_NAME'] = 'localhost'
 app.secret_key = 'unsecure-key-demo'
 
-app.config['KEYCLOAK'] = {
-    'consumer_key': 'flask-demo',
-    'consumer_secret': 'ff496a65-7455-4b45-b3ee-39ec31c3166e',
-}
-
-oauth = OAuth()
-oauth_provider = oauth.remote_app(
-    'fask-demo',
-    request_token_params={'scope': 'email'},
-    base_url='http://asuka.home.opencsi.com:8080/auth/realms/OpenCSI',
-    authorize_url='http://asuka.home.opencsi.com:8080/auth/realms/OpenCSI/protocol/openid-connect/auth',
-    access_token_url='http://asuka.home.opencsi.com:8080/auth/realms/OpenCSI/protocol/openid-connect/token',
-    app_key='KEYCLOAK'
-)  # type: OAuthRemoteApp
-oauth.init_app(app)
+keycloak = ClientRegistrationInfo(
+    client_id='flask-demo',
+    client_secret='ff496a65-7455-4b45-b3ee-39ec31c3166e'
+)
+config = ProviderConfiguration(
+    issuer='http://asuka.home.opencsi.com:8080/auth/realms/OpenCSI',
+    client_registration_info=keycloak
+)
+auth = OIDCAuthentication({'default': config}, app)
 
 
 @app.route('/')
@@ -28,18 +25,10 @@ def hello():
 
 
 @app.route('/login')
+@auth.oidc_auth('default')
 def login():
-    return oauth_provider.authorize(callback=url_for('authorized', _external=True))
-
-
-@app.route('/authorized')
-def authorized():
-    resp = oauth_provider.authorized_response()
-    if resp is None:
-        return 'Access denied: error=%s'
-    return jsonify(resp)
+    return 'hello world'
 
 
 if __name__ == '__main__':
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
-    app.run(host='localhost')
+    app.run(host='asuka.home.opencsi.com', port=5000)
